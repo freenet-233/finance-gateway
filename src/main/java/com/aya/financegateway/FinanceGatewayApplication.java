1e2ae651fac549eae081eae10ed8ee125af4ca7d
wangguangpeng123 <598970984@qq.com>
2021-09-11 09:05:13 +0800

    update .gitignore

---
+++ src/main/java/com/aya/financegateway/FinanceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.aya.financegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aya/financegateway/FinanceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aya.financegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
